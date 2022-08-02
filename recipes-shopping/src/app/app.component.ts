8de52756ea63e2fec65eed44bc0328a85cdf15be
Stewart Anoya <sanoya@pathwayscanada.ca>
2022-08-02 11:34:09 -0400

    add conditional rendering with event emitters

--- recipes-shopping/src/app/app.component.ts
+++ recipes-shopping/src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'recipes-shopping';
+  loadedFeature: string = 'recipe';
+  onNavigate(e) {
+    this.loadedFeature = e;
+  }
 }
